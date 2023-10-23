2adb0c39b1f3c89b5b31f3f8faccb7ba0d3e1c39
Sasikant Biswal <biswalsashikant@gmail.com>
2023-10-23 16:54:20 +0530

    Added Validation pipes and to handle global level validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Application Level Pipes... whenever a validation decorator found, run the validation
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
